18d92fccac59332f432a2547840e1365c89e1d8c
abhishekbu008 <abhishekbu30@gmail.com>
2023-06-09 19:25:46 +0530

    Added cmake project for clox

---
+++ clox/clox/clox.cpp
@@ -0,0 +1,13 @@
+﻿// clox.cpp : Defines the entry point for the application.
+//
+
+#include "clox.h"
+#include "headers/common.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
