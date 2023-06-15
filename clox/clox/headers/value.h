f39bbff36413a05765193f9a4f2e426280f03cfb
abhishekbu008 <abhishekbu30@gmail.com>
2023-06-15 18:44:05 +0530

    Added different types of values to vm

--- clox/clox/headers/value.h
+++ clox/clox/headers/value.h
@@ -3,7 +3,47 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
+
+#define IS_BOOL(value)		((value).type == VAL_BOOL)
+#define IS_NIL(value)		((value).type == VAL_NIL)
+#define IS_NUMBER(value)	((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)		((value).as.boolean)
+#define AS_NUMBER(value)	((value).as.number)
+
+inline Value BOOL_VAL(bool value) {
+    Value val;
+    val.type = VAL_BOOL;
+    val.as.boolean = value;
+    return val;
+}
+
+inline Value NIL_VAL() {
+    Value val;
+    val.type = VAL_NIL;
+    val.as.number = 0.0;
+    return val;
+}
+
+inline Value NUMBER_VAL(double value) {
+    Value val;
+    val.type = VAL_NUMBER;
+    val.as.number = value;
+    return val;
+}
 
 typedef struct {
 	int capacity;
@@ -11,6 +51,7 @@ typedef struct {
 	Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
