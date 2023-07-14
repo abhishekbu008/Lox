ce6bd2c086a1e1fb533d2ae954df7cc369389ac4
abhishekbu008 <abhishekbu30@gmail.com>
2023-07-14 17:40:53 +0530

    Added a garbage collector

--- clox/clox/object.c
+++ clox/clox/object.c
@@ -13,9 +13,16 @@
 static Obj* allocateObject(size_t size, ObjType type) {
 	Obj* object = (Obj*)reallocate(NULL, 0, size);
 	object->type = type;
+	object->isMarked = false;
 
 	object->next = vm.objects;
 	vm.objects = object;
+
+#ifndef DEBUG_LOG_GC
+	printf("%p allocate %zu for %d\n", (void*)object, size, type);
+#endif // !DEBUG_LOG_GC
+
+
 	return object;
 }
 
@@ -53,7 +60,10 @@ static ObjString* allocateString(char* chars, int length, uint32_t hash) {
 	string->length = length;
 	string->chars = chars;
 	string->hash = hash;
+
+	push(OBJ_VAL(string));
 	tableSet(&vm.strings, string, NIL_VAL);
+	pop();
 	return string;
 }
 
